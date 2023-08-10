1eed4d3f21a1c23190ff5c5a997eb4d4c3554d98
Po <qaws3588@gmail.com>
2023-08-10 21:43:55 +1000

    Initial Commit

---
+++ Budget Tracker/Budget_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_TrackerApp.swift
+//  Budget Tracker
+//
+//  Created by Wang Po on 10/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct Budget_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
